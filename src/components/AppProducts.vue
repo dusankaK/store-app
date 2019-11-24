982a73c46a97c0a7e1e48c2f4a801212a3ca371b
dusankaK <karacdusanka11@gmail>
2019-11-24 22:42:31 +0100

    Zadatak 5

--- src/components/AppProducts.vue
+++ src/components/AppProducts.vue
@@ -1,9 +1,33 @@
 <template>
-    <h1>AppProducts</h1>
+    <div>
+        <h3 class="my-4">Products</h3>
+        <hr/>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Quantity</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(product, id) in products" v-bind:key="id">
+                    <td>{{ product.name}}</td>
+                    <td>{{ product.quantity}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
+import { productService } from "../services/ProductService.js";
 export default {
+    data() {
+        return{
+            products: productService.list()
+        }
+        
+    }
     
 }
 </script>
