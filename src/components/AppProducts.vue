16f33340f06f939f6d9a7f82f68c300d14c57aac
dusankaK <karacdusanka11@gmail>
2019-11-25 00:16:58 +0100

    Zadatak 6

--- src/components/AppProducts.vue
+++ src/components/AppProducts.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
         <h3 class="my-4">Products</h3>
+
+        <i class="fas fa-search"></i> <input v-model="searchProduct" class="search" type="text" placeholder="Search products">
+       
         <hr/>
+        <div v-if="products.length > 0">
         <table class="table">
             <thead>
                 <tr>
@@ -10,12 +14,14 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="(product, id) in products" v-bind:key="id">
+                <tr v-for="product in products" :key="product.id">
                     <td>{{ product.name}}</td>
                     <td>{{ product.quantity}}</td>
                 </tr>
             </tbody>
         </table>
+        </div>
+        <h3 v-else>There is no products of that kind on the list.</h3>
     </div>
 </template>
 
@@ -24,14 +30,28 @@ import { productService } from "../services/ProductService.js";
 export default {
     data() {
         return{
-            products: productService.list()
+            products: productService.list(),
+            searchProduct: '',
+        }        
+    },
+
+    watch: {
+        searchProduct: function() {
+            return this.products = productService.list().filter(product => product.name.toLowerCase().includes(this.searchProduct.toLowerCase()))
         }
-        
     }
+    //includes vraca true ili false 
+ 
     
 }
 </script>
 
 <style scoped>
 
+.search {
+    border-radius: 5px; 
+    padding:5px;
+    margin-bottom:20px;
+
+}
 </style>
